0c068872fa5d3e2af656d2b54fcda8f67ffab711
NightMare489 <123az.az.Az.yaser@gmail.com>
2023-05-05 00:01:45 +0200

    done

--- x.py
+++ x.py
@@ -6,19 +6,6 @@ from svgwrite import cm, mm
 from PIL import Image, ImageDraw, ImageFont
 
 
-width = 2400
-height = 1000
-HeightInit = height 
-FirstX = 100
-VerticalDistance = 550
-
-dwg = svgwrite.Drawing('output2.svg', size=(width, height), profile='full')
-img = Image.new('RGB', (width, height), color='white')
-draw = ImageDraw.Draw(img)
-
-Lines = {}
-levels = []
-
 def generate_image(input_str):
     InputLines = input_str.split('\n')
     Title = InputLines[0]
@@ -30,7 +17,41 @@ def generate_image(input_str):
     dwg.add(dwg.text(Title, insert=(FirstX, FirstY - 25), fill='black', font_size=FontSize))
     x = FirstX
     for element in Elements:
+        if(elm:= re.findall(r'{(.*?)}', element)):
+            if(elm[0].startswith('(') and elm[0].endswith(')') or elm[0].startswith('_') and elm[0].endswith('_')):
+                Twidth, Theight = draw.textsize(elm[0][1:-1], font=Font)
+            else:
+                Twidth, Theight = draw.textsize(elm[0], font=Font)
+        else:
+            Twidth, Theight = draw.textsize(element, font=Font)
+        RectX = x
+        RectY = FirstY
+        RectW = max(Twidth + 40, 150)
+        RectH = 50
+
+        num = []
+        if '{' in element and '}' in element:
+            for item in element.split('{'):
+                if('}' in item):
+                    continue
+                for _ in item.split('-'):
+                    num.append(_)
+
+        if num:
+            for n in num:
+                b = False
+                if n[-1] == 'A':
+                    b = True
+                    n = int(n[0:-1])
+                else:
+                    n = int(n)
+                if n not in Lines:
+                    Lines[n] = [RectX,RectY,RectW,RectH,b]
+                else:
+                    Lines[n]+=[RectX,RectY,RectW,RectH,b]
         Underlined = False
+        if elm:
+            element = elm[0]
         if element.startswith('(') and element.endswith(')'):
             Underlined = True
             element = element[1:-1]
@@ -38,21 +59,6 @@ def generate_image(input_str):
         if element.startswith('_') and element.endswith('_'):
             Dotted = True
             element = element[1:-1]
-
-        Twidth, Theight = draw.textsize(element, font=Font)
-        RectX = x
-        RectY = FirstY
-        RectW = max(Twidth + 40, 150)
-        RectH = 50
-
-        # Check if the element has a number and store it in a dictionary
-        num = re.findall(r'^(\d+)\{.*\}$', element)
-        if num:
-            num = int(num[0])
-            if num not in Lines:
-                Lines[num] = [RectX,RectY,RectW,RectH]
-            else:
-                Lines[num]+=[RectX,RectY,RectW,RectH]
 
         rect = dwg.rect(insert=(RectX, RectY), size=(RectW, RectH), stroke='black', fill='none')
         dwg.add(rect)
@@ -77,68 +83,125 @@ def generate_image(input_str):
 
 def DrawLines():
     for key in Lines:
-        Rx1, Ry1, Rw1, Rh1, Rx2, Ry2, Rw2, Rh2 = Lines[key]
+        Rx1, Ry1, Rw1, Rh1,R1a, Rx2, Ry2, Rw2, Rh2,R2a = Lines[key]
         R1lvl = int(Ry1/(VerticalDistance/2))
         R2lvl = int(Ry2/ (VerticalDistance/2))
 
+        randStart = randint(5,FirstX-5)
+        
+        randR1x = randint(-int(Rw1/4), int(Rw1/4))
+        randR2x = randint(-int(Rw2/4), int(Rw1/4))
+
+        randRy = randint(-int((((VerticalDistance/2)-Rh1)/2)/2), int((((VerticalDistance/2)-Rh1)/2)/2))
+
         if(R2lvl-R1lvl == 1):
-            rand = randint(-80,80)
-            line1 = dwg.line(start=(Rx1+rand+Rw1/2, Ry1+Rh1), end=((Rx1+rand+Rw1/2), Ry1+rand+(Ry2-Ry1)/2), stroke='black', stroke_width=2)           
-            line2 = dwg.line(start=(Rx2+Rw2/2, Ry2), end=(Rx2+Rw2/2, Ry1+rand+(Ry2-Ry1)/2), stroke='black', stroke_width=2)
-            line3 = dwg.line(start=(Rx1+rand+Rw1/2, Ry1+rand+(Ry2-Ry1)/2), end=(Rx2+Rw2/2, Ry1+rand+(Ry2-Ry1)/2), stroke='black', stroke_width=2)
-            dwg.add(line1)
-            dwg.add(line2)
-            dwg.add(line3)
+            points = [
+                (Rx1+randR1x+Rw1/2, Ry1+Rh1),
+                (Rx1+randR1x+Rw1/2, randRy+(Ry1+Rh1)+((VerticalDistance/2)-Rh1)/2),
+                (Rx2+randR2x+Rw2/2, randRy+(Ry1+Rh1)+((VerticalDistance/2)-Rh1)/2),
+                (Rx2+randR2x+Rw2/2, Ry2)
+            
+            ]
         else:
-            rand = randint(10,FirstX-10)
             maxX = 0
             for i in range(R1lvl+1, R2lvl):
                 if levels[i-1] > maxX:
                     maxX = levels[i-1]
 
-            maxX+=rand
+            maxX += randint(10, 50)
 
-            distance = dist((Rx1+rand+Rw1/2, Ry1+Rh1),((Rx1+rand+Rw1/2), Ry1+rand+VerticalDistance/4))
-            distance+= dist((Rx2+Rw2/2, Ry2),(Rx2+Rw2/2, Ry2-VerticalDistance/4))
-            distance += dist((Rx1+rand+Rw1/2, Ry1+rand+VerticalDistance/4),(rand, Ry1+rand+VerticalDistance/4))
-            distance += dist((Rx2+Rw2/2, Ry2-VerticalDistance/4),(rand, Ry2-VerticalDistance/4))
-            distance += dist((rand, Ry1+rand+VerticalDistance/4),(rand, Ry2-VerticalDistance/4))
+            points1 = [
+                    (Rx1+randR1x+Rw1/2, Ry1+Rh1),
+                    (Rx1+randR1x+Rw1/2, randRy+(Ry1+Rh1)+((VerticalDistance/2)-Rh1)/2),
+                    (randStart, randRy+(Ry1+Rh1)+((VerticalDistance/2)-Rh1)/2),
+                    (randStart, randRy+(Ry2)-((VerticalDistance/2)-Rh2)/2),
+                    (Rx2+randR2x+Rw2/2, randRy+(Ry2)-((VerticalDistance/2)-Rh2)/2),
+                    (Rx2+randR2x+Rw2/2, Ry2),
+                ]
+            points2 = [
+                    (Rx1+randR1x+Rw1/2, Ry1+Rh1),
+                    (Rx1+randR1x+Rw1/2, randRy+(Ry1+Rh1)+((VerticalDistance/2)-Rh1)/2),
+                    (maxX, randRy+(Ry1+Rh1)+((VerticalDistance/2)-Rh1)/2),
+                    (maxX, randRy+(Ry2)-((VerticalDistance/2)-Rh2)/2),
+                    (Rx2+randR2x+Rw2/2, randRy+(Ry2)-((VerticalDistance/2)-Rh2)/2),
+                    (Rx2+randR2x+Rw2/2, Ry2),   
+                ]
             
-            distance2 = dist((Rx1+rand+Rw1/2, Ry1+Rh1),((Rx1+rand+Rw1/2), Ry1+rand+VerticalDistance/4))
-            distance2+= dist((Rx2+Rw2/2, Ry2),(Rx2+Rw2/2, Ry2-VerticalDistance/4))
-            distance2 += dist((Rx1+rand+Rw1/2, Ry1+rand+VerticalDistance/4),(maxX, Ry1+rand+VerticalDistance/4))
-            distance2 += dist((Rx2+Rw2/2, Ry2-VerticalDistance/4),(maxX, Ry2-VerticalDistance/4))
-            distance2 += dist((maxX, Ry1+rand+VerticalDistance/4),(maxX, Ry2-VerticalDistance/4))
+            distance1 =0
+            distance2 =0
+            for i in range(1,len(points1)):
+                distance1 += dist(points1[i],points1[i-1])
 
-            if distance < distance2:
-                line1 = dwg.line(start=(Rx1+rand+Rw1/2, Ry1+Rh1), end=((Rx1+rand+Rw1/2), Ry1+rand+VerticalDistance/4), stroke='black', stroke_width=2)   
-                line2 = dwg.line(start=(Rx2+Rw2/2, Ry2), end=(Rx2+Rw2/2, Ry2-VerticalDistance/4), stroke='black', stroke_width=2)
-                line3 = dwg.line(start=(Rx1+rand+Rw1/2, Ry1+rand+VerticalDistance/4), end=(rand, Ry1+rand+VerticalDistance/4), stroke='black', stroke_width=2)           
-                line4 = dwg.line(start=(Rx2+Rw2/2, Ry2-VerticalDistance/4), end=(rand, Ry2-VerticalDistance/4), stroke='black', stroke_width=2)
-                line5 = dwg.line(start=(rand, Ry1+rand+VerticalDistance/4), end=(rand, Ry2-VerticalDistance/4), stroke='black', stroke_width=2)
+            for i in range(1,len(points2)):
+                distance2 += dist(points2[i],points2[i-1])
+
+            if distance1 < distance2:
+                points = points1
             else:
-                line1 = dwg.line(start=(Rx1+rand+Rw1/2, Ry1+Rh1), end=((Rx1+rand+Rw1/2), Ry1+rand+VerticalDistance/4), stroke='black', stroke_width=2)   
-                line2 = dwg.line(start=(Rx2+Rw2/2, Ry2), end=(Rx2+Rw2/2, Ry2-VerticalDistance/4), stroke='black', stroke_width=2)
-                line3 = dwg.line(start=(Rx1+rand+Rw1/2, Ry1+rand+VerticalDistance/4), end=(maxX, Ry1+rand+VerticalDistance/4), stroke='black', stroke_width=2)           
-                line4 = dwg.line(start=(Rx2+Rw2/2, Ry2-VerticalDistance/4), end=(maxX, Ry2-VerticalDistance/4), stroke='black', stroke_width=2)
-                line5 = dwg.line(start=(maxX, Ry1+rand+VerticalDistance/4), end=(maxX, Ry2-VerticalDistance/4), stroke='black', stroke_width=2)
-              
-            dwg.add(line1)
-            dwg.add(line2)
-            dwg.add(line3)
-            dwg.add(line4)
-            dwg.add(line5)
+                points =points2
+
+        polyline = dwg.polyline(points=points, stroke='black', stroke_width=2,fill='none')
+        arrow = dwg.path(d="M 0 0 L 10 5 L 0 10 L 2.5 5 Z")
+
+        if R2a:
+            marker = dwg.marker(insert=(9.5, 5), size=(15, 15), orient='auto')
+            marker.add(arrow)
+            dwg.defs.add(marker)
+            polyline['marker-end'] = marker.get_funciri()
+        if R1a:
+            marker2 = dwg.marker(insert=(9.5, 5), size=(15, 15), orient='-90')
+            marker2.add(arrow)
+            dwg.defs.add(marker2)
+            polyline['marker-start'] = marker2.get_funciri()
+        dwg.add(polyline)
 
 
 
+# if __name__ == '__main__':
+#     with open('input.txt', 'r') as f:
+#         string = f.read()
+#     input_lines = string.split('\n')
+#     print(string)
+#     for i in range(0, len(input_lines), 2):
+#         generate_image(input_lines[i] + '\n' + input_lines[i+1])
+#         height += VerticalDistance
+#         dwg['height'] = (height) * mm
+#     DrawLines()
+#     dwg.save()
 
-if __name__ == '__main__':
-    with open('input.txt', 'r') as f:
-        string = f.read()
+def begin(string):
+    global height
+    global width
+    global HeightInit
+    global FirstX
+    global VerticalDistance
+    global dwg
+    global img
+    global draw
+    global Lines
+    global levels
+
+    height = 1000
+    width = 2400
+    HeightInit = height 
+    FirstX = 100
+    VerticalDistance = 350
+
+    dwg = svgwrite.Drawing('output2.svg', size=(width, height), profile='full')
+    img = Image.new('RGB', (width, height), color='white')
+    draw = ImageDraw.Draw(img)
+
+    Lines = {}
+    levels = []
+
     input_lines = string.split('\n')
+    print(string)
     for i in range(0, len(input_lines), 2):
         generate_image(input_lines[i] + '\n' + input_lines[i+1])
+
         height += VerticalDistance
         dwg['height'] = (height) * mm
     DrawLines()
-    dwg.save()
+    return dwg.tostring()
+
+    # dwg.save()
